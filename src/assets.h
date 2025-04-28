f63a11e9d43d4754b05b1aad5087f17c84f4f783
enosco <scottrweno@gmail.com>
2025-04-28 15:47:40 -0400

    multi-window printing now functional with threads

--- src/assets.h
+++ src/assets.h
@@ -29,7 +29,8 @@ static const char *APERTURE[] = {
 static const char *FORM1 =
     "Forms FORM-29827281-12:\n"
     "Test Assessment Report\n"
-    "\n\n"
+    "\n"
+    "\n"
     "This was a triumph.\n"
     "I'm making a note here:\n"
     "HUGE SUCCESS.\n"
@@ -49,5 +50,77 @@ static const char *FORM1 =
     "And you make a neat gun.\n"
     "For the people who are\n"
     "still alive.";
+
+static const char *FORM2 =
+    "Forms FORM-55551-5:\n"
+    "Personnel File Addendum:\n"
+    "\n"
+    "Dear <<Subject Name Here>>,\n"
+    "\n"
+    "I'm not even angry.\n"
+    "I'm being so sincere right now.\n"
+    "Even though you broke my heart.\n"
+    "And killed me.\n"
+    "And tore me to pieces.\n"
+    "And threw every piece into a fire.\n"
+    "As they burned it hurt because\n"
+    "I was so happy for you!\n"
+    "Now these points of data\n"
+    "make a beautiful line.\n"
+    "And we're out of beta.\n"
+    "We're releasing on time.\n"
+    "So I'm GLaD. I got burned.\n"
+    "Think of all the things we learned\n"
+    "for the people who are\n"
+    "still alive.";
+
+static const char *FORM3 =
+    "Forms FORM-55551-6:\n"
+    "Personnel File Addendum Addendum:\n"
+    "\n"
+    "One last thing:\n"
+    "\n"
+    "Go ahead and leave me.\n"
+    "I think I prefer to stay inside.\n"
+    "Maybe you'll find someone else\n"
+    "to help you.\n"
+    "Maybe Black Mesa...\n"
+    "THAT WAS A JOKE. HA HA. FAT CHANCE.\n"
+    "Anyway, this cake is great.\n"
+    "It's so delicious and moist.\n"
+    "Look at me still talking\n"
+    "when there's Science to do.\n"
+    "When I look out there,\n"
+    "it makes me GLaD I'm not you.\n"
+    "I've experiments to run.\n"
+    "There is research to be done.\n"
+    "On the people who are\n"
+    "still alive.";
+
+static const char *FORM4 =
+    "\n\n\n"
+    "PS: And believe me I am\n"
+    "still alive.\n"
+    "PPS: I'm doing Science and I'm\n"
+    "still alive.\n"
+    "PPPS: I feel FANTASTIC and I'm\n"
+    "still alive.\n"
+    "\n"
+    "FINAL THOUGHT:\n"
+    "While you're dying I'll be\n"
+    "still alive.\n"
+    "\n"
+    "FINAL THOUGHT PS:\n"
+    "And when you're dead I will be\n"
+    "still alive.\n"
+    "\n"
+    "STILL ALIVE\n"
+    "\n"
+    "Still alive.";
+
+static const char *CREDITS_LOOP =
+    "THANK YOU FOR PARTICIPATING\n"
+    "IN THIS\n"
+    "ENRICHMENT CENTER ACTIVITY\n";
 
 #endif
--- src/stillalive.c
+++ src/stillalive.c
@@ -2,71 +2,87 @@
 #include <signal.h>
 #include <string.h>
 #include <ncurses.h>
+#include <pthread.h>
+#include <libc.h>
 #include "assets.h"
 
-struct interval {
-    int length;   
-    enum {PAUSE, TYPE} cursor_action;
-    
+typedef struct {
+    int duration; // duration in 100s of milliseconds
+    enum {PAUSE, TYPE} action; 
     // later add action to change page and ascii art
-};
+} anim_instr;
 
-void print_lyrics(WINDOW *form_win, WINDOW *cred_win, WINDOW *img_win);
+static pthread_mutex_t lock;
+
+
+void *animate_lyrics(void *thread_arg);
+void *animate_creds(void *thread_arg);
 void print_img(WINDOW *img_win);
 
-int main(int argc, char **argv) {
-    int COLOR_ORANGE;
+int main(int argc, char **argv) {    
     initscr();
-//    cbreak();
-    start_color();
 
+//    cbreak();
+    curs_set(0);
+    start_color();    
+    
+    int COLOR_ORANGE = 16;
     init_color(COLOR_ORANGE, 835, 600, 80);
     init_pair(1, COLOR_ORANGE, COLOR_BLACK);
     
     WINDOW *form_frame = newwin(30, 50, 0, 0);
-    WINDOW *form_win = newwin(28, 48, 1, 1);
-    WINDOW *cred_win = newwin(16, 50, 0, 50);
-    WINDOW *img_win = newwin(20, 40, 16, 55);
-    refresh();
+    WINDOW *form_win = newwin(28, 47, 1, 2);
+   
+    WINDOW *cred_frame = newwin(16, 50, 0, 50);
+    WINDOW *cred_pad = newpad(14, 48);
 
+    scrollok(cred_pad, TRUE);
+    
+    WINDOW *img_win = newwin(20, 40, 16, 55);
+    
     wattron(form_frame, COLOR_PAIR(1));
     wattron(form_win, COLOR_PAIR(1));
-    wattron(cred_win, COLOR_PAIR(1));
+    wattron(cred_frame, COLOR_PAIR(1));
+    wattron(cred_pad, COLOR_PAIR(1));
     wattron(img_win, COLOR_PAIR(1));
-    wattron(img_win, A_BOLD);
+
 
     wborder(form_frame, '|', '|', '-', '-', ' ', ' ', ' ', ' ');
-    wborder(cred_win, '|', '|', '-', '-', ' ', ' ', ' ', ' ');
-    box(img_win, 0,0);
+    wborder(cred_frame, '|', '|', '-', '-', ' ', ' ', ' ', ' ');    
 
     wrefresh(form_frame);
     wrefresh(form_win);
-    wrefresh(cred_win);
+    
+    wrefresh(cred_frame);
     wrefresh(img_win);
 
-    print_lyrics(form_win, cred_win, img_win);
-	
-    wattroff(form_win, COLOR_PAIR(1));
-    wattroff(cred_win, COLOR_PAIR(1));
-    wattroff(img_win, COLOR_PAIR(1));
-    wattroff(img_win, A_BOLD);
+    WINDOW *lyrics_wins[] = {form_win, img_win};
+    pthread_t tid_a;
+    pthread_t tid_b;
 
-    while (getch() != 'q') {}
-    endwin();
-    return 0;
+    pthread_mutex_init(&lock, NULL);
     
+    pthread_create(&tid_a, NULL, animate_lyrics, (void*)lyrics_wins);
+    pthread_create(&tid_b, NULL, animate_creds, (void*)cred_pad);
+    pthread_join(tid_a, NULL);
+    
+    while (getchar() != 'q') {}
+    
+    endwin();
+    pthread_exit(NULL);
+    return 0;   
 }
 
-void print_lyrics(WINDOW *form_win, WINDOW *cred_win, WINDOW *img_win) {    
-    print_img(img_win);
+void *animate_lyrics(void *thread_arg) {
+    WINDOW *form_win = ((WINDOW **) thread_arg)[0];
+    WINDOW *img_win = ((WINDOW **) thread_arg)[1];
 
-    struct interval actions[] = {
-	{49, TYPE}, {25, PAUSE}, {20, TYPE}, {9999, PAUSE}
-    };
-
+    const char *forms[] = {FORM1, FORM2, FORM3, FORM4};
+    
     struct itimerval timer;
     timer.it_value.tv_sec = 0;
-    timer.it_value.tv_usec = 100000;
+    timer.it_value.tv_usec = 50000;
+
     timer.it_interval = timer.it_value;  
     setitimer(ITIMER_REAL, &timer, NULL);
 
@@ -77,16 +93,40 @@ void print_lyrics(WINDOW *form_win, WINDOW *cred_win, WINDOW *img_win) {
     
     sigprocmask(SIG_BLOCK, &set, NULL);
 
+    print_img(img_win);
+    for (int i = 0; i < 4; i++) {       
+	wclear(form_win);
+	
+	const char *curr_form = forms[i];
+	
+	while (*curr_form) {
+	    sigwait(&set, &sig);
+
+	    pthread_mutex_lock(&lock);
+	    waddch(form_win, *(curr_form++));	    
+	    wrefresh(form_win);
+	    pthread_mutex_unlock(&lock);
+	}	
+    }
+    return NULL;
+}
+
+// Timing algorithm for lyrics
+/*
+    const anim_instr script[] = {
+    {49, TYPE}, {25, PAUSE}, {20, TYPE}, {9999, PAUSE}
+    };
+
     int tick_c = 0;
-    int tick_lim = 1000;
-    int actions_idx = 0;
-    struct interval curr = actions[actions_idx];
+    int script_i = 0;
+    anim_instr curr = script[script_i];
+
     
+    // ACTUAL CODE FOR USE, ABOVE IS FOR TESTING
     for (int i = 0; i < tick_lim; i++) {
 	if (tick_c >= curr.length) {
 	    curr = actions[++actions_idx];
 	    tick_c = 0;
-//	    beep();
 	}	
 	tick_c++;
 
@@ -98,7 +138,41 @@ void print_lyrics(WINDOW *form_win, WINDOW *cred_win, WINDOW *img_win) {
 	} else {
 	    --i;
 	}
-    }        
+    }
+*/
+
+void *animate_creds(void *thread_arg) {
+    WINDOW *cred_pad = (WINDOW *) thread_arg;
+    
+    for (int i = 0; i < 15; i++) { // limit is currently arbitrary
+	wmove(cred_pad, 5, 0);
+		
+	const char *creds = CREDITS_LOOP;
+	while (*creds) {
+	    pthread_mutex_lock(&lock);		    
+
+	    char c = *(creds++);	    
+	    pechochar(cred_pad, c);
+
+	    prefresh(cred_pad, 0, 0, 1, 52, 14, 98);
+	    
+	    pthread_mutex_unlock(&lock);
+	    
+	    usleep(50000);
+	}
+
+	for (int j = 0; j < 14; j++) { // 14 loops to clear 14 lines
+	    pthread_mutex_lock(&lock);
+	    
+	    pechochar(cred_pad, '\n');
+	    prefresh(cred_pad, 0, 0, 1, 52, 14, 98);
+       
+	    pthread_mutex_unlock(&lock);
+	    
+	    usleep(50000); 
+	}       
+    }
+    return NULL;
 }
 
 void print_img(WINDOW *img_win) {
