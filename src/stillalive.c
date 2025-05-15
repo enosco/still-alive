eb45e7f4ed926bf219baddc9a8b3f0469b836515
enosco <scottrweno@gmail.com>
2025-05-15 12:16:22 -0400

    audio playback now functional

--- src/stillalive.c
+++ src/stillalive.c
@@ -4,26 +4,28 @@
 #include <pthread.h>
 #include <signal.h>
 #include <sys/time.h>
+#include "miniaudio.h"
 #include "assets.h"
 
 typedef struct {
-    int duration; // duration in 100s of milliseconds
-    enum {PAUSE, TYPE} action; 
-    // later add action to change page and ascii art
+    enum {TYPE, PAUSE} action;
+    int remaining; // only relevant for PAUSE actions
+//  int target_img;
 } anim_instr;
 
-static pthread_mutex_t lock;
+//static pthread_mutex_t lock;
 
 
-void *animate_lyrics(void *thread_arg);
-void *animate_creds(void *thread_arg);
+//void *animate_lyrics(void *thread_arg);
+//void *animate_creds(void *thread_arg);
+void animate(WINDOW *form_win, WINDOW *img_win, WINDOW* cred_pad);
 void print_img(WINDOW *img_win);
 
 int main(int argc, char **argv) {
 
     initscr();
     //cbreak;
-    curs_set(0);
+    //curs_set(0);
     start_color();
     
     int COLOR_ORANGE = 16;
@@ -57,7 +59,7 @@ int main(int argc, char **argv) {
     wrefresh(img_win);
 
     /* SETUP INTERVAL TIMER AND SIGNAL HANDLER */
-    
+    /*
     int sig;
     sigset_t set;
     sigemptyset(&set);
@@ -70,8 +72,24 @@ int main(int argc, char **argv) {
     timer.it_value.tv_usec = 100000;    
     timer.it_interval = timer.it_value;  
     setitimer(ITIMER_REAL, &timer, NULL);        
+    */
+    /* START AUDIO */
 
+    ma_engine engine;
+    ma_result result;
+    ma_sound sound;
+
+    ma_engine_init(NULL, &engine);
+    ma_sound_init_from_file(&engine, "stillalive.mp3", 0, NULL, NULL, &sound);
+
+    ma_sound_set_volume(&sound, 0.5);
+    ma_sound_set_start_time_in_milliseconds(&sound, 5000);
+    ma_sound_start(&sound);
+
+    print_img(img_win);
+    animate(form_win, img_win, cred_pad);
     /* BEGIN THREADS */
+    /*
     print_img(img_win);
     
     pthread_t tid_a;
@@ -86,7 +104,7 @@ int main(int argc, char **argv) {
     
     pthread_join(tid_a, NULL);
     pthread_join(tid_b, NULL);
-    
+    */
     while (getchar() != 'q') {}
     
     endwin();
@@ -94,7 +112,81 @@ int main(int argc, char **argv) {
     return 0;
 }
 
-void *animate_lyrics(void *thread_arg) {
+
+void animate(WINDOW *form_win, WINDOW *img_win, WINDOW* cred_pad) {
+    int sig;
+    sigset_t set;
+    sigemptyset(&set);
+    sigaddset(&set, SIGALRM);
+
+    sigprocmask(SIG_BLOCK, &set, NULL);
+
+    struct itimerval timer;
+    timer.it_value.tv_sec = 0;
+    timer.it_value.tv_usec = 100000;    
+    timer.it_interval = timer.it_value;  
+    setitimer(ITIMER_REAL, &timer, NULL);        
+
+    const char *creds = CREDITS_LOOP;   
+    const int START_LINE = 11;
+    const int SCROLL_C = 4;
+    int scroll_d = SCROLL_C;
+   
+    const char *forms[] = {FORM1, FORM2, FORM3, FORM4};
+    
+    int tick_c = 0;
+    int script_i = 0;
+    
+    const anim_instr script[] = {
+	{TYPE, -1}, {PAUSE, 999}
+    };
+    anim_instr curr_instr = script[script_i];
+    
+    wmove(cred_pad, START_LINE, 0);
+    for (int i = 0; i < 4; i++) {
+	const char *curr_form = forms[i];
+	
+	while (*curr_form) {
+	    sigwait(&set, &sig);
+
+
+	    /* CREDITS WINDOW */
+	    if (*creds) {
+		pechochar(cred_pad, *(creds++));
+	    } else {
+		if (scroll_d > 0) {
+		    pechochar(cred_pad, '\n');
+		    scroll_d--;		    
+		} else {
+		    wmove(cred_pad, START_LINE, 0);
+		    creds = CREDITS_LOOP;
+		    scroll_d = SCROLL_C;
+		}
+	    }
+
+	    /* LYRICS WINDOW */
+	    char lyr_c = *(curr_form++);
+
+	    if (lyr_c == ';' || curr_instr.remaining == 0) {
+		curr_instr = script[++script_i];
+	    }
+	    
+	    if (curr_instr.action == TYPE) {
+		waddch(form_win, lyr_c);
+	    } else {
+		curr_instr.remaining--;
+	    }
+	   
+	    
+	    prefresh(cred_pad, 0, 0, 1, 52, 14, 98);	    
+	    wrefresh(form_win);		    
+
+	}
+	wclear(form_win);		
+    }
+}
+/*
+  void *animate_lyrics(void *thread_arg) {
     WINDOW *form_win = ((WINDOW **) thread_arg)[0];
     WINDOW *img_win = ((WINDOW **) thread_arg)[1];
 
@@ -121,18 +213,19 @@ void *animate_lyrics(void *thread_arg) {
     }
     return NULL;
 }
-
+*/
 // Timing algorithm for lyrics
 /*
-    const anim_instr script[] = {
-    {49, TYPE}, {25, PAUSE}, {20, TYPE}, {9999, PAUSE}
+  
+  const anim_instr script[] = {
+	{49, TYPE}, {25, PAUSE}, {20, TYPE}, {9999, PAUSE}
     };
 
     int tick_c = 0;
     int script_i = 0;
     anim_instr curr = script[script_i];
 
-    
+
     // ACTUAL CODE FOR USE, ABOVE IS FOR TESTING
     for (int i = 0; i < tick_lim; i++) {
 	if (tick_c >= curr.length) {
@@ -151,7 +244,7 @@ void *animate_lyrics(void *thread_arg) {
 	}
     }
 */
-
+/*
 void *animate_creds(void *thread_arg) {
     WINDOW *cred_pad = (WINDOW *) thread_arg;
     
@@ -189,7 +282,8 @@ void *animate_creds(void *thread_arg) {
     }
     return NULL;
 }
-
+*/
+    
 void print_img(WINDOW *img_win) {
     for (int line = 0; line < 20; line++) {
 	mvwprintw(img_win, line, 0, APERTURE[line]);	
